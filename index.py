89338780a20104906436a37a2d3e6bc8e6145cdd
Lucas São Bernardo <bernardo.pinheiro@hotmail.com>
2024-01-11 18:04:26 -0300

    melhoradinha no codigo

--- index.py
+++ index.py
@@ -1,13 +1,22 @@
 from PIL import Image
+import os
 
-def thumbnail(img_name, img_save):
-  image = Image.open(img_name)
-  image.thumbnail((512,512))
-  image.save(img_save)
+def thumbnail(img_name, img_save, height = 1024, width= 1024):
+  if os.path.isfile(img_name):
+    image = Image.open(img_name)
+    image.thumbnail((height, width))
+    image.save(img_save)
+    print('Logo criada')
+  else:
+    print('Imagem não encontrada')
 
-def resize(img_name, img_save):
-  image = Image.open(img_name)
-  new_image = image.resize((512,512))
-  new_image.save(img_save)
+def resize(img_name, img_save, height = 1024, width= 1024):
+  if os.path.isfile(img_name):
+    image = Image.open(img_name)
+    new_image = image.resize((height, width))
+    new_image.save(img_save)
+    print('Logo criada')
+  else:
+    print('Imagem não encontrada')
 
-resize('Academia Campestre.png', '20304_logo1.png')
+resize('Endurance.jpg', '21621_logo1.png')
